4b01f783f0cd1db0c18263b0034699d12972097a
Dmitriy1965 <grek00@mail.ru>
2023-05-19 23:11:19 +0300

    first commit

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(7777);) { // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+            try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ) {
+                // ваш код
+                out.println("пароль");
+                System.out.println("New connection accepted");
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+
